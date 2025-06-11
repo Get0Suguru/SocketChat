1f55f761218b0855aeabb9b2b4716841c8ce8e93
Gautam Taneja <gautam45taneja@gmail.com>
2025-06-11 22:15:44 +0530

    tiny dto for signup/login flow (username + password)

---
+++ src/main/java/com/suguru/geto/socket/chat/payload/UserRequest.java
@@ -0,0 +1,15 @@
+package com.suguru.geto.socket.chat.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRequest {
+    private String username;
+    private String password;
+
+
+}
